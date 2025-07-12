5341a0bd10063b7e8daff1af37c195c5bca62979
Carlosfelipe21 <cfcfcosta21@gmail.com>
2025-07-12 09:07:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let numero_01 = Number(prompt("Digite o Primeiro Numero: "))
+let numero_02 = Number(prompt("Digite o Segundo Numero: "))
+
+let resultado = numero_01
+resultado += numero_02
+console.log("Soma: " + resultado)
+
+resultado = numero_01
+resultado -= numero_02
+console.log("Subtração: " + resultado)
+
+resultado = numero_01
+resultado *= numero_02
+console.log("Multiplicação: " + resultado)
+
+resultado = numero_01
+resultado /= numero_02
+console.log("Divisão: " + resultado)
+
+resultado = numero_01
+resultado %= numero_02
+console.log("Resto da divisão: " + resultado)
+
